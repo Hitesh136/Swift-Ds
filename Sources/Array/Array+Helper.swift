2d4e3347aebdf1ccd497d0ea0749adf63f95659c
Hitesh Agarwal <agarwal.hitesh94@gmail.com>
2019-03-05 07:15:36 +0530

    Create Program of subarray with given sum

--- Sources/Array/Array+Helper.swift
+++ Sources/Array/Array+Helper.swift
@@ -38,3 +38,31 @@ extension Array where Element == Int {
         return arr
     }
 }
+
+extension Array where Element: Numeric, Element: Comparable {
+    
+    /*
+     Subarray with given sum
+     https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0
+     */
+    
+    func subArray(withSum sum: Element) -> [Element] {
+        guard !self.isEmpty else {
+            return []
+        }
+        var total = self.first!
+        var lastIndex = 0
+        for i in 1..<self.count {
+            let ele = self[i]
+            total += ele
+            if total > sum {
+                total -= self[lastIndex]
+                lastIndex += 1
+            }
+            if total == sum {
+                return Array(self[lastIndex...i])
+            } 
+        }
+        return []
+    }
+}
--- Sources/Array/ArrayTestCase.swift
+++ Sources/Array/ArrayTestCase.swift
@@ -12,5 +12,17 @@ class ArrayTestCase: XCTestCase {
     func test2_NextLargerElement() {
         XCTAssertEqual( [4, 5, 2, 25].nextLargerElement(), [5, 25, 25, -1])
     }
+}
 
+//Subarray with given sum
+extension ArrayTestCase {
+    func test_1_SubArrayWithSum() {
+        let arr = [1, 2, 3, 7, 5]
+        XCTAssertEqual(arr.subArray(withSum: 12), [2, 3, 7])
+    }
+    
+    func test_2_SubArrayWithSum() {
+        let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        XCTAssertEqual(arr.subArray(withSum: 15), [1, 2, 3, 4, 5])
+    }
 }
