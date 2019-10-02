83dba7e8357e28c52fe9784115682f9c46259603
Hitesh Agarwal <agarwal.hitesh94@gmail.com>
2019-10-02 22:20:14 +0530

    Implement Combinations of words from integer in an array using both string and tree.

--- Swift Command Line/Array+Helper.swift
+++ Swift Command Line/Array+Helper.swift
@@ -171,3 +171,63 @@ extension Array where Element: Numeric, Element: Comparable {
         return res
     }
 }
+
+
+//https://www.techiedelight.com/combinations-of-words-formed-replacing-given-numbers-corresponding-english-alphabet/
+extension Array where Element == Int {
+	func getPossibleStringsFromNumber(_ index: Index,_ node: BinaryNode<String>?) {
+		
+		guard let node = node, index < count else {
+			return
+		}
+		var resultString = node.value
+		
+		let intValue = self[index]
+		resultString.append(getString(fromASCIINumber: intValue))
+		node.rightChild = BinaryNode(value: resultString)
+		getPossibleStringsFromNumber(index + 1, node.rightChild)
+		
+		//		var results: BinaryNode<Int>? = getPossibleStringsFromNumber(index + 1, resultString)
+		
+		if index < count - 1 {
+			let jointValue = intValue * 10 + (self[index +  1])
+			if jointValue < 27 {
+				var _resultString = node.value
+				_resultString.append(getString(fromASCIINumber: jointValue))
+				node.leftChild = BinaryNode(value: _resultString)
+				getPossibleStringsFromNumber(index + 2, node.leftChild)
+			}
+		}
+	} 
+	
+	func getString(fromASCIINumber intValue: Int) -> String {
+		if let unicodeValue = UnicodeScalar(intValue + 64)  {
+			return String(unicodeValue)
+		}
+		return ""
+	}
+	
+	func getPossibleStringsFromNumber(_ index: Index,_ resultStringArg: String) -> [String] {
+		
+		var resultString = resultStringArg
+		guard index < count else {
+			return [resultString]
+		}
+		
+		let intValue = self[index]
+		resultString.append(getString(fromASCIINumber: intValue))
+		var results = getPossibleStringsFromNumber(index + 1, resultString)
+		
+		if index < count - 1 {
+			let jointValue = intValue * 10 + (self[index +  1])
+			if jointValue < 27 {
+				var _resultString = resultStringArg
+				_resultString.append(getString(fromASCIINumber: jointValue))
+				let _results = getPossibleStringsFromNumber(index + 2, _resultString)
+				results.append(contentsOf: _results)
+			}
+		}
+		
+		return results
+	}
+}
--- Swift Command Line/main.swift
+++ Swift Command Line/main.swift
@@ -1,1 +1,22 @@
 
+
+import Foundation
+
+
+//let rootNode = BinaryNode(value: 80)
+//rootNode.leftChild = BinaryNode(value: 50)
+//rootNode.rightChild = BinaryNode(value: 90)
+//rootNode.leftChild?.leftChild = BinaryNode(value: 10)
+//rootNode.leftChild?.rightChild = BinaryNode(value: 60)
+//
+//rootNode.rightChild?.leftChild = BinaryNode(value: 40)
+//rootNode.rightChild?.rightChild = BinaryNode(value: 20)
+
+
+var numbers = [1, 2, 2]
+var rootNode = BinaryNode(value: "")
+numbers.getPossibleStringsFromNumber(0, rootNode)
+print(rootNode.asString)
+print("Resluts: \(rootNode.getLeafValues())")
+
+
